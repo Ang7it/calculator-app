a9935ba20694c61e2b4714249d0ead9f1597b84a
Angshit <angshitghosh@gmail.com>
2020-12-26 13:41:51 +0530

    Green test for test 3

--- src/main/java/com/tdd/demo/service/Calculator.java
+++ src/main/java/com/tdd/demo/service/Calculator.java
@@ -5,7 +5,11 @@ public class Calculator {
 	public static int add(String input) {
 		if(input.isEmpty()) {
 			return 0;
-		}else {
+		}else if(input.contains(",")) {
+			String[] sar=input.split(",");
+			return Integer.valueOf(sar[0])+Integer.valueOf(sar[1]);
+		}
+		else {
 			return Integer.valueOf(input);
 		}
 	}
